0f18d8d00f1f7609a6a5a95b4ec12d526b9b4ec1
vivaan <xapqrt@vivaans-MacBook-Air-2.local>
2026-05-22 20:43:00 +0530

    voxel extraction working, capturing levers and buttons

--- src/main/java/com/xapqrt/redstonetoreal/RedstoneScanner.java
+++ src/main/java/com/xapqrt/redstonetoreal/RedstoneScanner.java
@@ -1,13 +1,18 @@
 package com.xapqrt.redstonetoreal;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RedstoneScanner {
     
     // fabric API documentation is literally hiding this from me
     
-    
+    // store inputs found in the world
+    public List<BlockPos> raw_inputs = new ArrayList<>();
     
     
     
@@ -25,8 +30,15 @@ public class RedstoneScanner {
         
         for (int x = 0; x < 16; x++) {
             for (int z = 0; z < 16; z++) {
-                BlockPos block_thing = startPos.add(x, 0, z);
-                // just a placeholder for now
+                for (int y = -64; y < 320; y++) {
+                    BlockPos block_thing = startPos.add(x, y, z);
+                    BlockState current_state = world.getBlockState(block_thing);
+                    
+                    if (current_state.isOf(Blocks.LEVER) || current_state.isOf(Blocks.STONE_BUTTON) || current_state.isOf(Blocks.OAK_BUTTON)) {
+                        System.out.println("FOUND INPUT FUCK");
+                        raw_inputs.add(block_thing);
+                    }
+                }
             }
         }
     }
