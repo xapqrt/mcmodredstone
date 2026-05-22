270357a889d9ee60f3bad87f869b4af14bfb084d
vivaan <xapqrt@vivaans-MacBook-Air-2.local>
2026-05-22 20:43:15 +0530

    basic mapping for redstone torches and repeaters, facing property extracted

--- src/main/java/com/xapqrt/redstonetoreal/RedstoneScanner.java
+++ src/main/java/com/xapqrt/redstonetoreal/RedstoneScanner.java
@@ -2,7 +2,11 @@ package com.xapqrt.redstonetoreal;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.RedstoneTorchBlock;
+import net.minecraft.block.RepeaterBlock;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +17,9 @@ public class RedstoneScanner {
     
     // store inputs found in the world
     public List<BlockPos> raw_inputs = new ArrayList<>();
+    
+    // logic components
+    public List<String> comp_cache = new ArrayList<>();
     
     
     
@@ -37,6 +44,18 @@ public class RedstoneScanner {
                     if (current_state.isOf(Blocks.LEVER) || current_state.isOf(Blocks.STONE_BUTTON) || current_state.isOf(Blocks.OAK_BUTTON)) {
                         System.out.println("FOUND INPUT FUCK");
                         raw_inputs.add(block_thing);
+                    }
+                    
+                    if (current_state.isOf(Blocks.REDSTONE_TORCH) || current_state.isOf(Blocks.REDSTONE_WALL_TORCH)) {
+                        boolean is_lit = current_state.get(Properties.LIT);
+                        Direction facing = current_state.contains(Properties.HORIZONTAL_FACING) ? current_state.get(Properties.HORIZONTAL_FACING) : Direction.UP;
+                        comp_cache.add("NOT_GATE:" + block_thing.toShortString() + ":facing=" + facing.getName() + ":lit=" + is_lit);
+                    }
+                    
+                    if (current_state.isOf(Blocks.REPEATER)) {
+                        Direction facing = current_state.get(Properties.HORIZONTAL_FACING);
+                        int delay = current_state.get(RepeaterBlock.DELAY);
+                        comp_cache.add("BUFFER:" + block_thing.toShortString() + ":facing=" + facing.getName() + ":delay=" + delay);
                     }
                 }
             }
